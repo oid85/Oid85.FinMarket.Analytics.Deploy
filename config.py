76ad81d9ca5ca9453bba8665b67fc95145a13ed4
oid85 <oid85@mail.ru>
2025-12-01 21:53:27 +0700

    .

---
+++ config.py
@@ -0,0 +1,7 @@
+deploy_path = 'C:\\Deploy'
+
+finmarket_analytics_source_path = 'C:\\Repository\\Oid85.FinMarket.Analytics\\Oid85.FinMarket.Analytics'
+finmarket_analytics_deploy_directory = 'Oid85.FinMarket.Analytics'
+finmarket_analytics_exe_file_name = "Oid85.FinMarket.Analytics.WebHost.exe"
+finmarket_analytics_service_name = 'Oid85.FinMarket.Analytics'
+finmarket_analytics_appsettings_file = 'appsettings.json'
---
+++ main.py
@@ -0,0 +1,7 @@
+import oid85_finmarket_analytics
+
+if __name__ == '__main__':
+    steps = [1]
+
+    if 1 in steps:
+        oid85_finmarket_analytics.deploy()
